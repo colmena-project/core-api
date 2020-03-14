ca334adc060dd0acc7528a00571ffd09f5befdc7
Santiago Semhan <santiagosemhan@gmail.com>
2020-03-14 14:20:25 -0300

    Update Post class

--- cloud/classes/Post.js
+++ cloud/classes/Post.js
@@ -1,8 +1,40 @@
 const Base = require('./Base');
+const AccountService = require('../services/AccountService');
 
 class Post extends Base {
   constructor() {
-    super(Post.prototype.constructor.name);
+    super(Post.name);
+  }
+
+  static async afterSave(request) {
+    const { object } = request;
+    const account = await object.getOwnerAccount();
+    if (account) {
+      const nickname = account.get('nickname');
+      object.set('nickname', nickname);
+    }
+    return object;
+  }
+
+  static async afterFind(request) {
+    const { objects: posts } = request;
+    const postPromisses = posts.map((post) => post.getOwnerAccount());
+
+    const ownersAccount = await Promise.all(postPromisses);
+    ownersAccount.forEach((account, index) => {
+      if (account) {
+        const nickname = account.get('nickname');
+        posts[index].set('nickname', nickname);
+      }
+    });
+
+    return posts;
+  }
+
+  async getOwnerAccount() {
+    const createdBy = this.get('createdBy');
+    const account = await AccountService.findAccountByUser(createdBy);
+    return account;
   }
 }
 
