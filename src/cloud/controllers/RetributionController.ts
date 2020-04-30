9bbbe5ff5a472ab674eba3a619b049da4821ba5c
Santiago Semhan <santiagosemhan@gmail.com>
2020-04-30 19:39:17 -0300

    Change transport calculation response. Type code.

--- src/cloud/controllers/RetributionController.ts
+++ src/cloud/controllers/RetributionController.ts
@@ -3,20 +3,32 @@ import RetributionService from '../services/RetributionService';
 import ContainerService from '../services/ContainerService';
 import MapService from '../services/MapService';
 
-const processMaterialInput = async (elements: string[], user: Parse.User) => {
+const processMaterialInput = async (
+  elements: string[],
+  user: Parse.User,
+): Promise<{ total: number; elements: { containerId: string; value: number }[] }> => {
   // get containers
-  const containers = await Promise.all(elements.map((e) => ContainerService.findContainerById(e, user)));
+  const containers = await Promise.all(
+    elements.map((e) => ContainerService.findContainerById(e, user)),
+  );
   // get retribution for each one
-  const materialsRetributions = await Promise.all(containers.map((c) => {
-    const materials = [{
-      container: c,
-      qty: c.get('type').get('qty'),
-      unit: c.get('type').get('unit'),
-    }];
-    return RetributionService.getMaterialRetribution(materials);
-  }));
+  const materialsRetributions = await Promise.all(
+    containers.map((c) => {
+      const materials = [
+        {
+          container: c,
+          qty: c.get('type').get('qty'),
+          unit: c.get('type').get('unit'),
+        },
+      ];
+      return RetributionService.getMaterialRetribution(materials);
+    }),
+  );
   // set material elements response
-  const materialDetail = materialsRetributions.map((value, index) => ({ containerId: containers[index].id, value }));
+  const materialDetail = materialsRetributions.map((value, index) => ({
+    containerId: containers[index].id,
+    value,
+  }));
   // set result
   const totalResult = materialDetail.reduce((accumulator, detail) => accumulator + detail.value, 0);
   return {
@@ -25,16 +37,22 @@ const processMaterialInput = async (elements: string[], user: Parse.User) => {
   };
 };
 
-const processTransportInput = async (origin: LatLng, destination: LatLng) => {
+const processTransportInput = async (
+  origin: LatLng,
+  destination: LatLng,
+): Promise<{ total: number }> => {
   const distanceMatrix = await MapService.distancematrix(origin, destination);
   const distance = distanceMatrix.distance[0].elements[0];
   const kms = distance.distance.value / 1000;
-  return RetributionService.getTransportRetribution(kms);
+  const total = await RetributionService.getTransportRetribution(kms);
+  return {
+    total,
+  };
 };
 
 const estimateRetribution = async (request: Parse.Cloud.FunctionRequest) => {
-  const { params, user } = <{ params: Parse.Cloud.Params, user: Parse.User }>request;
-  const { type, elements = [] } = <{ type: string, elements: string[]}> params;
+  const { params, user } = <{ params: Parse.Cloud.Params; user: Parse.User }>request;
+  const { type, elements = [] } = <{ type: string; elements: string[] }>params;
   let response;
   let unsoportedType;
   let materialResult;
