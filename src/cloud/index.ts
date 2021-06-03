e3e1f975f1db1016130f1d72ad501eb40706abf0
Santiago Semhan <santiagosemhan@gmail.com>
2021-06-03 13:55:23 -0300

    Add domain logic to login algorithm

--- src/cloud/index.ts
+++ src/cloud/index.ts
@@ -6,8 +6,23 @@ loadClassHooks();
 const legacyMode = true;
 loadCloudFunctions(legacyMode);
 
-Parse.Cloud.beforeLogin((request: Parse.Cloud.TriggerRequest) => {
-  const { object: user } = request;
+Parse.Cloud.beforeLogin(async (request: Parse.Cloud.TriggerRequest) => {
+  const { object: user, headers } = request;
+
+  const domainUUID = headers['X-Requested-With'];
+
+  if (domainUUID) {
+    const domain = await new Parse.Query('Domain')
+      .include('role')
+      .equalTo('uuid', domainUUID)
+      .first();
+    if (!domain) throw new Error('Invalid domain');
+    const role = <Parse.Object>domain.get('role');
+    const users = <Parse.Relation>role.get('users');
+    const existingUser = await users.query().get(user.id);
+    if (!existingUser) throw new Error('User is not allowed to login in domain');
+  }
+
   if (user.get('isBanned')) {
     throw new Error('Access denied, your account is disabled.');
   }
