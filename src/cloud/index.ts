e9419515fd58353b401c5589372a1aaaf0ef959e
Santiago Semhan <santiagosemhan@gmail.com>
2021-06-03 19:46:52 -0300

    Fix x-requested-with header check

--- src/cloud/index.ts
+++ src/cloud/index.ts
@@ -9,7 +9,7 @@ loadCloudFunctions(legacyMode);
 Parse.Cloud.beforeLogin(async (request: Parse.Cloud.TriggerRequest) => {
   const { object: user, headers } = request;
 
-  const domainUUID = headers['X-Requested-With'];
+  const domainUUID = headers['x-requested-with'];
 
   if (domainUUID) {
     const domain = await new Parse.Query('Domain')
@@ -19,8 +19,11 @@ Parse.Cloud.beforeLogin(async (request: Parse.Cloud.TriggerRequest) => {
     if (!domain) throw new Error('Invalid domain');
     const role = <Parse.Object>domain.get('role');
     const users = <Parse.Relation>role.get('users');
-    const existingUser = await users.query().get(user.id);
-    if (!existingUser) throw new Error('User is not allowed to login in domain');
+    try {
+      await users.query().get(user.id);
+    } catch (error) {
+      throw new Error('User is not allowed to login in the requested domain');
+    }
   }
 
   if (user.get('isBanned')) {
