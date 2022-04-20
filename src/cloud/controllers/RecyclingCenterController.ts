50c17c51e22b08ecad23bd1e2cc94b015a9ec911
Fernando Andrés Hillebrand <fer.hill@gmail.com>
2022-04-20 09:23:25 -0300

    add to recycling center the walletId and walletToken

--- src/cloud/controllers/RecyclingCenterController.ts
+++ src/cloud/controllers/RecyclingCenterController.ts
@@ -12,16 +12,35 @@ const createRecyclingCenter = async (
   request: Colmena.SecureFunctionRequest,
 ): Promise<Parse.Object> => {
   const { params, user } = request;
-  const { name, description, latLng } = params;
-  return await RecyclingCenterService.createRecyclingCenter({ name, description, latLng }, user);
+  const { name, description, latLng, walletId, walletToken } = params;
+  return await RecyclingCenterService.createRecyclingCenter(
+    {
+      name,
+      description,
+      latLng,
+      walletId,
+      walletToken,
+    },
+    user,
+  );
 };
 
 const editRecyclingCenter = async (
   request: Colmena.SecureFunctionRequest,
 ): Promise<Parse.Object> => {
   const { params, user } = request;
-  const { id, name, description, latLng } = params;
-  return await RecyclingCenterService.editRecyclingCenter({ id, name, description, latLng }, user);
+  const { id, name, description, latLng, walletId, walletToken } = params;
+  return await RecyclingCenterService.editRecyclingCenter(
+    {
+      id,
+      name,
+      description,
+      latLng,
+      walletId,
+      walletToken,
+    },
+    user,
+  );
 };
 
 const deleteRecyclingCenter = async (request: Colmena.SecureFunctionRequest): Promise<boolean> => {
